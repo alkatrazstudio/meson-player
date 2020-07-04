3f2032238958f1bfe2a01b8761dfea4b5281a1f8
ZXED <zxed@alkatrazstudio.net>
2020-07-04 17:30:00 +0300

    Add OSX CI build

---
+++ setup/ci/build/build-osx.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/env bash
+set -ex
+cd "$(dirname -- "${BASH_SOURCE[0]}")/../../.."
+
+
+#
+# Check that all vars are set
+#
+set +x
+[[ -z $EXTERNAL_DATA_URL ]] && (echo 'env not set: $EXTERNAL_DATA_URL'; exit -1)
+[[ -z $EXTERNAL_DATA_PASSWORD ]] && (echo 'env not set: $EXTERNAL_DATA_URL'; exit -1)
+set -x
+
+
+#
+# Install the required packages
+#
+brew install p7zip megatools
+
+
+#
+# Install Qt
+#
+sudo mkdir -p /opt/qt
+pushd /opt/qt
+    curl -SsL 'https://github.com/alkatrazstudio/qt5-build/releases/download/v5.13.2-0/qt5-v5.13.2-osx-x86_64.tar.xz' | sudo tar -xJ
+popd
+
+
+#
+# Install external data
+#
+set +x
+megadl --path=./external.7z --no-progress --no-ask-password "$EXTERNAL_DATA_URL"
+7za x -bso0 external.7z -p"$EXTERNAL_DATA_PASSWORD"
+set -x
+
+sudo cp external/bass/osx/libbass.dylib /usr/local/lib/
+sudo cp external/bass/osx/libbassmix.dylib /usr/local/lib/
+cp -a external/bass/osx/plugins ./
+
+if [[ -d external/services ]]
+then
+    cp -a external/services ./
+fi
+
+rm -rf external.7z external
+
+
+#
+# Build
+#
+setup/build.sh -v
+mv /tmp/build/meson-player/pkg/*.pkg ./
