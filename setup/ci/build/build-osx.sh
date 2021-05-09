6e9b7b90fbb7e68a07ad3261b104a1fb65d9bd80
ZXED <zxed@alkatrazstudio.net>
2021-05-09 14:07:39 +0300

    Fix access rights

--- setup/ci/build/build-osx.sh
+++ setup/ci/build/build-osx.sh
@@ -37,8 +37,8 @@ popd
 #
 sudo mkdir -p /opt/icu
 pushd /opt/icu
-    curl -SsL "$ICU_URL" | tar -xJ
-    cp -al icu/. /opt/qt/qt5/.
+    curl -SsL "$ICU_URL" | sudo tar -xJ
+    sudo cp -al icu/. /opt/qt/qt5/.
 popd
 sudo rm -rf /opt/icu
 
