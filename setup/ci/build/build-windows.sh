c92688e4430fb1ea82f881af9b757c459147842b
ZXED <zxed@alkatrazstudio.net>
2021-05-09 14:07:50 +0300

    Fix unarchiving

--- setup/ci/build/build-windows.sh
+++ setup/ci/build/build-windows.sh
@@ -47,7 +47,7 @@ rm -rf qtpkg
 #
 mkdir -p /opt/icu
 pushd /opt/icu
-    curl -SsL "$ICU_URL" | tar -xJ
+    curl -SsL "$ICU_URL" | unxz | tar -xf-
     cp -a icu/. /mingw64/opt/qt5alst/.
 popd
 rm -rf /opt/icu
